8c356667b0046283d4bdd46843049cc07e99f5e0
jarod.w <wl.jarod@gmail.com>
2019-07-27 13:38:22 +0800

    Log for get_port_by_name
    Signed-off-by: jarod.w <wl.jarod@gmail.com>

--- array_lbaasv2_driver/common/plugin_rpc.py
+++ array_lbaasv2_driver/common/plugin_rpc.py
@@ -420,11 +420,14 @@ class ArrayLoadBalancerCallbacks(object):
     def get_port_by_name(self, context, port_name=None):
         """Get port by name."""
         if port_name:
-            filters = {'name': [port_name]}
-            return self.driver.plugin.db._core_plugin.get_ports(
-                context,
-                filters=filters
-            )
+            with context.session.begin(subtransactions=True):
+                filters = {'name': [port_name]}
+                ret_ports = self.driver.plugin.db._core_plugin.get_ports(
+                    context,
+                    filters=filters
+                )
+                LOG.debug("get_port_by_name: --%s-- returned" % ret_ports)
+                return ret_ports
 
 
     @log_helpers.log_method_call
